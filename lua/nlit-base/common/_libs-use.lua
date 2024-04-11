dbfb1eacbe576961141225257f1e5cf0c32d292f
CodeLit <nlit@pm.me>
2024-04-11 19:49:56 +0500

    WIP

--- lua/nlit-base/common/_libs-use.lua
+++ lua/nlit-base/common/_libs-use.lua
@@ -1,32 +1,37 @@
+--- The libfiner is an importer module for nlit libraries. Global shared module.
+-- @module nlitLibs
+-- @usage local libs = nlitLibs
+-- @see nlit
+module('nlitLibs', package.seeall)
+
 local tobool = tobool
 local string = string
 local PrintError = PrintError
 local include = include
 local pairs = pairs
 local file = file
----The libfiner will find CW libs for you. Global shared module.
----@module Libs
+
 local LibFinder = {}
 
 local function IsStringContains(str, text)
     return tobool(string.find(str, text))
 end
 
-local pathToLibs = 'cyber-libs'
+local pathToLibs = 'nlit-libs'
 
----Use CW lib. Basic fucntion.
----@param libName string
----@return table lib
----@usage local colors = CWGUI.Colors
---- Button:SetBackgroundColor(colors:White())
---- local Strings = CW:Lib('strings')
---- ...
-function CW:Lib(libName)
+--- Using of Nlit lib. Basic fucntion.
+-- @param libName string
+-- @return table lib
+-- @usage local libs = nlitLibs
+-- local colors = libs.Colors
+-- Button:SetBackgroundColor(colors:White())
+-- local Strings = libs:Lib('strings')
+-- ...
+function Lib(libName)
     local found = LibFinder:FindLibPathInFolder(pathToLibs, string.lower(libName))
 
     if not found then
-        -- PrintError('Не найдена библиотека [' .. path .. '] в файле ' .. NFiles:curPath()() .. '!')
-        PrintError('Не найдена библиотека [' .. libName .. ']!')
+        PrintError('[Nlit\'s Framework] Library [' .. libName .. '] is not found!')
 
         return
     end
@@ -34,20 +39,20 @@ function CW:Lib(libName)
     return include(found)
 end
 
-function LibFinder:FindLuaLibInFiles(files, lib)
+function FindLuaLibInFiles(files, lib)
     for _, f in pairs(files) do
         if IsStringContains(f, '.lua') and IsStringContains(f, lib) then return f end
     end
 end
 
-function LibFinder:LookupForLib(folders, path, lib)
+function LookupForLib(folders, path, lib)
     for _, folder in pairs(folders) do
         local found = self:FindLibPathInFolder(path .. '/' .. folder, lib)
         if found then return found end
     end
 end
 
-function LibFinder:FindLibPathInFolder(path, lib)
+function FindLibPathInFolder(path, lib)
     local files, folders = file.Find(path .. '/*', 'LUA')
     local found = self:FindLuaLibInFiles(files, lib)
     if found then return path .. '/' .. found end
@@ -55,5 +60,5 @@ function LibFinder:FindLibPathInFolder(path, lib)
     return self:LookupForLib(folders, path, lib)
 end
 
-CW:AddAllInFolder(pathToLibs, true)
+AddAllInFolder(pathToLibs, true)
 
