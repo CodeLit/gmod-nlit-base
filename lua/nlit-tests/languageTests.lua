5c25df64b972241e24e20b9cfcece2d9f8883c61
CodeLit <nlit@pm.me>
2024-04-13 20:55:26 +0500

    WIP

--- lua/nlit-tests/languageTests.lua
+++ lua/nlit-tests/languageTests.lua
@@ -1,4 +1,4 @@
 local lang = nlitLang
 local test = nlitTest
 test:assertIsEqual(lang:Get('Save items'), 'Сохранить предметы')
-lang:print('[Nlit Framework][TEST] Language test passed!')
+lang:print('[Nlit\'s Framework][TEST] Language test passed!')
--- lua/nlit-tests/stringsUTF8Tests.lua
+++ lua/nlit-tests/stringsUTF8Tests.lua
@@ -3,4 +3,4 @@ local strings = nlitString
 -- test:assertIsEqual(strings:Capitalize('привет мир!'), 'Привет Мир!')
 -- test:assertIsEqual(strings:Lower('Некотораястрока с МАЛЕНЬКИМИ буквамИ'), 'некотораястрока с маленькими буквами')
 -- test:assertIsEqual(strings:Upper('некотораястрока с большими буквами'), 'НЕКОТОРАЯСТРОКА С БОЛЬШИМИ БУКВАМИ')
-print('[Nlit Framework][TEST] UTF8 Strings test passed!')
+print('[Nlit\'s Framework][TEST] UTF8 Strings test passed!')
