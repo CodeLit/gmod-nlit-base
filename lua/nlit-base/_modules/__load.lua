f48c1220f26278da225a63981767fa101e3b1c5b
CodeLit <nlit@pm.me>
2024-04-15 22:47:45 +0500

    fix

--- lua/nlit-base/_modules/__load.lua
+++ lua/nlit-base/_modules/__load.lua
@@ -1,18 +1,15 @@
 --- Global shared module. Allows run code on start, and reload it on save
 -- @module nlitLoad
--- @usage local load = nlitLoad
--- load:EnsureLoad('MyHook',0,function() print('Hello') end)
 module('nlitLoad', package.seeall)
-
 local isfunction = isfunction
 local timer = timer
 local hook = hook
-
 --- Load code after entities was initialized. Global function.
 -- Former deprecated name is DefinetlyLoad
 -- @param hookName string unique name for future removal
 -- @param loadTime number [optional] - delay for loading code
 -- @param doCode function
+-- @usage nlitLoad.EnsureLoad('MyHook',0,function() print('Hello') end)
 function EnsureLoad(hookName, loadTime, doCode)
     if isfunction(loadTime) then
         doCode = loadTime
@@ -31,6 +28,7 @@ end
 -- @param hookName string unique name for future removal
 -- @param loadTime number [optional] - delay for loading code
 -- @param doCode function
+-- @usage nlitLoad.WhenGMLoaded('MyHook',0,function() print('Hello') end)
 function WhenGMLoaded(hookName, loadTime, doCode)
     if isfunction(loadTime) then
         doCode = loadTime
@@ -38,9 +36,7 @@ function WhenGMLoaded(hookName, loadTime, doCode)
     end
 
     local function DoIfGM()
-        if GAMEMODE then
-            doCode()
-        end
+        if GAMEMODE then doCode() end
     end
 
     local function Run()
@@ -48,7 +44,6 @@ function WhenGMLoaded(hookName, loadTime, doCode)
     end
 
     DoIfGM()
-
     if CLIENT then
         -- TODO: Оптимизировать так, чтобы таймер был один, но хуки вызывались все
         timer.Create('NCommon GM Loading ' .. hookName, 0.05, 0, function()
